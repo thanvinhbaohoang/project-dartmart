4c4f57345b2269df5e8eaf243847f86783546ee5
burkejaeger <81589723+burkejaeger@users.noreply.github.com>
2022-05-22 20:28:33 -0400

    Added About page styling

--- youtube-search-app/App.js
+++ youtube-search-app/App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import MainTabBar from './navigation/main_tab_bar';
+import { LogBox } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// disable really annoying in app warnings
+LogBox.ignoreAllLogs();
+const App = (props) => {
+  return <MainTabBar />;
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default App;
