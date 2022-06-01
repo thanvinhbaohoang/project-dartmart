038e381dcaa873f6c16e47f821079670f92009d1
camdenhao <camden.hao@gmail.com>
2022-06-01 17:07:57 -0400

    merge conflicts

--- src/screens/cartPage.js
+++ src/screens/cartPage.js
@@ -4,6 +4,62 @@ import { StyleSheet, Image, Text, View, TouchableOpacity, Dimensions, ScrollView
 import { LinearGradient } from 'expo-linear-gradient';
 import { addItem, submitOrder, removeItem } from '../actions/index';
 
+function CheckoutScreen() {
+    const { initPaymentSheet, presentPaymentSheet } = useStripe();
+    const [loading, setLoading] = useState(false);
+  
+    const fetchPaymentSheetParams = async () => {
+      const response = await fetch(`${API_URL}/payment-sheet`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const { paymentIntent, ephemeralKey, customer } = await response.json();
+  
+      return {
+        paymentIntent,
+        ephemeralKey,
+        customer,
+      };
+    };
+  
+    const initializePaymentSheet = async () => {
+      const {
+        paymentIntent,
+        ephemeralKey,
+        customer,
+      } = await fetchPaymentSheetParams();
+  
+      const { error } = await initPaymentSheet({
+        customerId: customer,
+        customerEphemeralKeySecret: ephemeralKey,
+        paymentIntentClientSecret: paymentIntent,
+      });
+      if (!error) {
+        setLoading(true);
+      }
+    };
+  
+    const openPaymentSheet = async () => {
+      // see below
+    };
+  
+    useEffect(() => {
+      initializePaymentSheet();
+    }, []);
+  
+    return (
+      <Screen>
+        <Button
+          variant="primary"
+          disabled={!loading}
+          title="Checkout"
+          onPress={openPaymentSheet}
+        />
+      </Screen>
+    );
+  }
 function CartPage(props){
     const cart = useSelector((state) => state.item.cart);
 
