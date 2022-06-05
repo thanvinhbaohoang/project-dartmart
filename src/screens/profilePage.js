083c20efd725c56ea2d8d03332644c3a79f2bfad
burkejaeger <81589723+burkejaeger@users.noreply.github.com>
2022-06-05 18:04:22 -0400

    update profile pag

--- src/screens/profilePage.js
+++ src/screens/profilePage.js
@@ -1,41 +1,53 @@
 import React, { useEffect, useState } from 'react';
 import { connect, useSelector } from 'react-redux';
-import { StyleSheet, Text, View, TouchableOpacity, Dimensions, ScrollView, Modal, Pressable, Button} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions, ScrollView, Modal, Pressable, Button, Alert} from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Ionicons } from "@expo/vector-icons";
 import { updateUser } from '../actions/index';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 
 function ProfilePage(props){
 
     const user = useSelector(state => state.user.user);
 
+    const futureFeatureAlertPayment = () => {
+        Alert.alert('Coming Soon!', 'Users will be able to update payment info from settings.');
+    }
+
+    const futureFeatureAlertPersonalInfo = () => {
+        Alert.alert('Coming Soon!', 'Users will be able to show photo and change name on order tickets.');
+    }
+
     return (
         <View backgroundColor='red' style={styles.container}>
 
                 <Text style={styles.featuredText}> {user?.name}</Text>
-                <Text style={styles.text2}> {orderDetail.role}</Text>
+                {user?.isDriver === false ? <Text style={styles.text2}> Customer </Text> :  
+                <Text style={styles.text2}> Driver </Text> }
                 <View style={styles.buttonsSection}>
-                    <TouchableOpacity style={styles.profileButton}>
-                        <Ionicons name='person' size={50} color={'white'}/>
+                    <TouchableOpacity style={styles.profileButton} onPress={futureFeatureAlertPersonalInfo}>
+                        <AntDesign name="user" size={50} color="whitesmoke" />
                         <Text style={styles.text1}>Personal Info</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.profileButton}>
-                        <Ionicons name='card' size={50} color={'white'}/>
+                    <TouchableOpacity style={styles.profileButton} onPress={futureFeatureAlertPayment}>
+                        <Ionicons name='card' size={50} color={'whitesmoke'}/>
                         <Text style={styles.text1}>Payment</Text>
                     </TouchableOpacity>
 
                     <TouchableOpacity onPress={props.route.params.logout} style={styles.profileButton}>
-                        <Ionicons name='log-out' size={50} color={'white'}/>
+                        <MaterialIcons name="logout" size={50} color="whitesmoke" />
                         <Text style={styles.text1}>Logout</Text>
                     </TouchableOpacity>
 
                     {user?.isDriver === false ? <TouchableOpacity onPress={() => props.updateUser(user.id, {isDriver: true})} style={styles.profileButton}>
-                        <Ionicons name='car' size={50} color={'white'}/>
+                        <MaterialCommunityIcons name="account-switch-outline" size={50} color="whitesmoke" />
                         <Text style={styles.text1}>Driver View</Text>
                     </TouchableOpacity> : null}
                     {user?.isDriver === true ? <TouchableOpacity onPress={() => props.updateUser(user.id, {isDriver: false})} style={styles.profileButton}>
-                        <Ionicons name='arrow-back' size={50} color={'white'}/>
+                        <MaterialCommunityIcons name="account-switch-outline" size={50} color="whitesmoke" />
                         <Text style={styles.text1}>Customer View</Text>
                     </TouchableOpacity> : null}
                 </View>
@@ -47,7 +59,7 @@ const windowHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
     text1: {
-        color: 'white',
+        color: 'whitesmoke',
         fontSize: 24,
         fontWeight: 'bold',
         alignSelf: 'center',
@@ -55,7 +67,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center'
     },
     text2: {
-        color: 'white',
+        color: 'whitesmoke',
         fontSize: 20,
         fontWeight: 'normal',
       },
@@ -71,7 +83,7 @@ const styles = StyleSheet.create({
     },
     featuredText: {
         marginTop:20,
-        color: 'white',
+        color: 'whitesmoke',
         fontSize: 36,
         fontWeight: 'bold',
         textAlign: 'center'
