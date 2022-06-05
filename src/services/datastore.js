bae3249ea38d92dcaaa3be065f227cccd0feb117
camdenhao <camden.hao@gmail.com>
2022-06-05 00:37:45 -0400

    changes

--- src/services/datastore.js
+++ src/services/datastore.js
@@ -6,7 +6,7 @@ import { initializeApp } from "firebase/app";
 
 // FIREBASE CONFIGURATION
 
-const API_URL = "http://localhost:3000";
+const API_URL = "https://stripeserver.onrender.com";
 
   // firebase config object
   const firebaseConfig = {
@@ -87,14 +87,14 @@ const API_URL = "http://localhost:3000";
       return { ...response.docs[0].data(), id: response.docs[0].id };
     } else {
        console.log('pulling customer')
-      //  const customer = await axios.post(`${API_URL}/v1/customers`, 
-      //   {
-      //     email: data.email,
-      //     name: data.name,
-      //   },
-      // );
-      // console.log("customer found:", customer.data)
-      const tempDoc = await setDoc(doc(db, "users", newUserId), {...data, id: newUserId, stripeId: 100});
+       const customer = await axios.post(`${API_URL}/v1/customers`, 
+        {
+          email: data.email,
+          name: data.name,
+        },
+      );
+      console.log("customer found:", customer.data)
+      const tempDoc = await setDoc(doc(db, "users", newUserId), {...data, id: newUserId, stripeId: customer.data.id});
       // console.log('created user:', tempDoc);
       return tempDoc;
       // return null
